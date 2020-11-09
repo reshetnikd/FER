464bcf99edf0d0a6c598d58251bde43a8b91c18c
reshetnikd <dmitry.reshetnik@icloud.com>
2020-11-09 22:27:52 +0200

    Add draft UI scheme

--- FER/ViewController.swift
+++ FER/ViewController.swift
@@ -8,10 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var currencyLabel: UILabel!
+    private let refreshControl: UIRefreshControl = UIRefreshControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        currencyLabel.layer.cornerRadius = currencyLabel.frame.width / 2.0
+        currencyLabel.clipsToBounds = true
+        tableView.refreshControl = refreshControl
+        refreshControl.addTarget(self, action: #selector(refreshData(_:)), for: .valueChanged)
+    }
+    
+    @objc private func refreshData(_ sender: Any) {
+        // Fetch Data
+        print("Fetch data")
     }
 
 
